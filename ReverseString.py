5759d7e09b26faf7de9f98443ac70116994d1a2a
Harsh Vardhan <hpandit1804@gmail.com>
2025-06-27 22:43:15 +0530

    reverse string

--- ReverseString.py
+++ ReverseString.py
@@ -1,2 +1,14 @@
 def ReverseString(text):
-    
+    return text[::-1]
+
+def ReverseString2(text):
+    text = reversed(text)
+    return ''.join(text)
+
+def ReverseString3(text):
+    for i in range(len(text)-1,-1,-1):
+       print(text[i], end="")
+    return ''
+
+
+print(ReverseString3("harsh"))
